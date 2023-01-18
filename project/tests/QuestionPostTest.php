51c9aaf9acde37eaef2147cb02cec70f0ae1df58
Flavien AUBERT <flavien.aubert.dev@gmail.com>
2023-01-18 17:04:37 +0100

    add unit tests
    question and answer unit test

--- project/tests/QuestionPostTest.php
+++ project/tests/QuestionPostTest.php
@@ -2,6 +2,8 @@
 
 namespace App\Tests;
 
+use App\Entity\Question;
+use App\Entity\Status;
 use PHPUnit\Framework\TestCase;
 
 class QuestionPostTest extends TestCase
@@ -13,16 +15,44 @@ class QuestionPostTest extends TestCase
 
     public function testIsTrue()
     {
+        $question = new Question();
 
+        $question
+            ->setTitle("title")
+            ->setPromoted(true)
+            ->setStatus(Status::Draft)
+            ->setCreatedAt(new \DateTime('now'))
+        ;
+        $this->assertTrue($question->getTitle() === "title");
+        $this->assertTrue($question->isPromoted() === true);
+        $this->assertTrue($question->getCreatedAt() === new \DateTime('now'));
+        $this->assertTrue($question->getStatus() === Status::Draft);
     }
 
     public function testIsFalse()
     {
+        $question = new Question();
 
+        $question
+            ->setTitle("title")
+            ->setPromoted(true)
+            ->setStatus(Status::Draft)
+            ->setCreatedAt(new \DateTime('now'))
+        ;
+
+        $this->assertFalse($question->getTitle() === "dabou");
+        $this->assertFalse($question->isPromoted() === false);
+        $this->assertFalse($question->getCreatedAt() === new \DateTime("2000-12-21"));
+        $this->assertFalse($question->getStatus() === Status::Published);
     }
 
     public function testIsEmpty()
     {
+        $question = new Question();
 
+        $this->assertEmpty($question->getTitle());
+        $this->assertEmpty($question->getCreatedAt());
+        $this->assertEmpty($question->getStatus());
+        $this->assertEmpty($question->isPromoted());
     }
 }
